9b060c84f9344c31865de105ddeac5b0f8427c3b
eladkorngut <elad.korngut@mail.huji.ac.il>
2023-11-21 15:17:56 +0200

    Some quality of life changes. Removed some commented lines and added a plot for the paths. Also the current parameter works well for a bimodal network with eps=0.1, but only for the x1 coordinate

--- catastrophe.py
+++ catastrophe.py
@@ -9,18 +9,6 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 
-peak_location = lambda lam: -2*((( (lam-1)*(1-12*lam+3*lam**2)+8*(lam**2)*np.log(lam) )/(4*lam**3))-(1-1/lam)**2)/(1-1/lam)**2/2
-
-
-def eq_points_exact(epsilon,beta,gamma):
-    # for 2d bimodal case find the eq points
-    epsilon_lam, epsilon_mu,lam = epsilon[0], epsilon[1],beta/gamma
-    y1star=(-2*epsilon_mu*(1 + epsilon_lam*epsilon_mu)+ lam*(-1 + epsilon_mu)*(1 + (-1 + 2*epsilon_lam)*epsilon_mu)+ np.sqrt(lam**2 +epsilon_mu*(4*epsilon_mu +lam**2*epsilon_mu*(-2 +epsilon_mu**2) +4*epsilon_lam*(lam -(-2 + lam)*epsilon_mu**2) +4*epsilon_lam**2*epsilon_mu*(lam -(-1 + lam)*epsilon_mu**2))))/(4*lam*(-1 +epsilon_lam)*(-1 +epsilon_mu)*epsilon_mu)
-    y2star=(lam + epsilon_mu*(-2 + 2*lam +lam*epsilon_mu+ 2*epsilon_lam*(lam +(-1 + lam)*epsilon_mu)) -np.sqrt(lam**2 +epsilon_mu*(4*epsilon_mu +lam**2*epsilon_mu*(-2 +epsilon_mu**2) +4*epsilon_lam*(lam -(-2 + lam)*epsilon_mu**2) +4*epsilon_lam**2*epsilon_mu*(lam -(-1 + lam)*epsilon_mu**2))))/(4*lam*(1 +epsilon_lam)*epsilon_mu*(1 + epsilon_mu))
-    p1star=-np.log((lam + 2*epsilon_lam -epsilon_lam**2*(lam -2*epsilon_mu) +np.sqrt(lam**2 +4*lam*epsilon_lam*epsilon_mu- 4*(-2 + lam)*epsilon_lam**3*epsilon_mu+ epsilon_lam**4*(lam**2 -4*(-1 + lam)*epsilon_mu**2) +epsilon_lam**2*(4 - 2*lam**2 + 4*lam*epsilon_mu**2)))/(2*(1 +epsilon_lam)*(1 +epsilon_lam*epsilon_mu)))
-    p2star= -np.log(-(lam - 2*epsilon_lam- epsilon_lam**2*(lam + 2*epsilon_mu) +np.sqrt(lam**2 + 4*lam*epsilon_lam*epsilon_mu- 4*(-2 + lam)*epsilon_lam**3*epsilon_mu+ epsilon_lam**4*(lam**2 - 4*(-1 + lam)*epsilon_mu**2) + epsilon_lam**2*(4 - 2*lam**2 + 4*lam*epsilon_mu**2)))/(2*(-1 + epsilon_lam)*(1 + epsilon_lam*epsilon_mu)))
-    return y1star, y2star, 0.0, 0.0, p1star, p2star
-
 
 hamiltonian = lambda y1,y2,p1,p2,epsilon,R0:((-1 + np.exp(-p1)) * y1 + (-1 + np.exp(-p2)) * y2 -(R0 * (-2 + y1 + y2 +
             np.exp(p1) * (-1 + y1) * (-1 + epsilon) - y1 * epsilon + y2 * epsilon -np.exp(p2) * (-1 + y2) *
@@ -38,30 +26,6 @@ def eq_points_mj(epsilon,R0):
     p2star = np.log(2) - np.log((R0*(-1 + epsilon**2) + 2*(epsilon + epsilon**3) -np.sqrt(R0**2*(-1 + epsilon**2)**2 +
               4*(epsilon + epsilon**3)**2 -4*R0*epsilon**2*(-1 + epsilon**4)))/((-1 + epsilon)*(1 + epsilon**2)))
     return y1star,y2star,0.0,0.0,p1star,p2star
-
-def eq_hamilton_J(beta,time,epsilon=0.0,factor=1.0,gamma=1.0,duration=1.0,t0=5.0):
-    def bimodal_mu_lam(q,t,lam,eps,factor,g,duration,t0):
-        epsilon_lam, epsilon_mu = eps[0], eps[1]
-        dy1_dt_sus_inf =lambda l: l * ((1 - epsilon_lam) * q[0] + (1 + epsilon_lam) * q[1]) * (1 - epsilon_mu) * (
-                1 / 2 - q[0]) * np.exp(q[2]) - g * q[0] * np.exp(-q[2])
-        dy2_dt_sus_inf = lambda l: l * ((1 - epsilon_lam) * q[0] + (1 + epsilon_lam) * q[1]) * (1 + epsilon_mu) * (
-                1 / 2 - q[1]) * np.exp(q[3]) - g * q[1] * np.exp(-q[3])
-        dtheta1_dt_sus_inf = lambda l: -l * (1 - epsilon_lam) * (
-                (1 - epsilon_mu) * (1 / 2 - q[0]) * (np.exp(q[2]) - 1) + (1 + epsilon_mu) * (1 / 2 - q[1]) * (
-                np.exp(q[3]) - 1)) + l * ((1 - epsilon_lam) * q[0] + (1 + epsilon_lam) * q[1]) * (
-                                               1 - epsilon_mu) * (np.exp(q[2]) - 1) - g * (np.exp(-q[2]) - 1)
-        dtheta2_dt_sus_inf = lambda l: -l * (1 + epsilon_lam) * (
-                (1 - epsilon_mu) * (1 / 2 - q[0]) * (np.exp(q[2]) - 1) + (1 + epsilon_mu) * (1 / 2 - q[1]) * (
-                np.exp(q[3]) - 1)) + l * ((1 - epsilon_lam) * q[0] + (1 + epsilon_lam) * q[1]) * (
-                                    1 + epsilon_mu) * (np.exp(q[3]) - 1) - g * (np.exp(-q[3]) - 1)
-
-        dy1_dt = dy1_dt_sus_inf(lam*factor) if t>t0 and t<t0+duration else dy1_dt_sus_inf(lam)
-        dy2_dt= dy2_dt_sus_inf(lam*factor) if t>t0 and t<t0+duration else dy2_dt_sus_inf(lam)
-        dp1_dt = dtheta1_dt_sus_inf(lam*factor) if t>t0 and t<t0+duration else dtheta1_dt_sus_inf(lam)
-        dp2_dt = dtheta2_dt_sus_inf(lam*factor) if t>t0 and t<t0+duration else dtheta2_dt_sus_inf(lam)
-        return np.array([dy1_dt, dy2_dt, dp1_dt, dp2_dt])
-    y1_0, y2_0, p1_0, p2_0, p1_star_clancy, p2_star_clancy = eq_points_exact(epsilon, beta*(1+epsilon[0]*epsilon[1]), gamma)
-    return y1_0, y2_0, p1_0, p2_0, p1_star_clancy, p2_star_clancy, bimodal_mu_lam, ndft.Jacobian(bimodal_mu_lam)
 
 
 def eq_motion_mj(lam,t0,tf,xi,epsilon):
@@ -91,11 +55,9 @@ def postive_eigen_vec(J,q0):
     return postive_eig_vec
 
 
-def shoot(y1_0, y2_0, p1_0, p2_0, tshot, J,dq_dt,lam,epsilon,factor,gamma,qstar):
+def shoot(y1_0, y2_0, p1_0, p2_0, tshot, J,dq_dt,qstar):
     q0 = (y1_0, y2_0, p1_0, p2_0)
     vect_J = lambda q, tshot: J(q0,0.0)
-    # qsol = odeint(dq_dt, q0, tshot,atol=1.0e-20, rtol=1.0e-13, mxstep=1000000000, hmin=1e-30, Dfun=vect_J)
-    # qsol = odeint(dq_dt, q0, tshot,args=(lam,epsilon,factor,gamma,duration,t0),Dfun=vect_J,tcrit=qstar)
     qsol = odeint(dq_dt, q0, tshot,Dfun=vect_J,tcrit=qstar)
     return qsol
 
@@ -110,14 +72,49 @@ def one_shot(shot_angle,lin_weight,q_star,radius,final_time_path,one_shot_dt,J,s
         , q0[2] + float(postive_eig_vec[0][2]) * one_shot_dt + (1 - lin_weight) * float(postive_eig_vec[1][2]) * one_shot_dt \
         , q0[3] + lin_weight * float(postive_eig_vec[0][3]) * one_shot_dt + (1 - lin_weight) * float(
         postive_eig_vec[1][3]) * one_shot_dt
-    # y1_i, y2_i, p1_i, p2_i =  q0[0] + lin_weight * float(postive_eig_vec[0][0]) * one_shot_dt + (
-    #             1 - lin_weight) * float(postive_eig_vec[1][0]) * one_shot_dt \
-    #     , q0[1] + float(lin_weight * postive_eig_vec[0][1]) * one_shot_dt + (1 - lin_weight) * float(
-    #     postive_eig_vec[1][1]) * one_shot_dt \
-    #     , q0[2] + float(postive_eig_vec[0][2]) * one_shot_dt + (1 - lin_weight) * float(postive_eig_vec[1][2]) * one_shot_dt \
-    #     , -2.1e-12+q0[3] + lin_weight * float(postive_eig_vec[0][3]) * one_shot_dt + (1 - lin_weight) * float(
-    #     postive_eig_vec[1][3]) * one_shot_dt
-    return shoot(y1_i, y2_i, p1_i, p2_i, final_time_path,J,shot_dq_dt,lam,epsilon,factor,gamma,[0.0,final_time_path[-1]])
+    return shoot(y1_i, y2_i, p1_i, p2_i, final_time_path,J,shot_dq_dt,[0.0,final_time_path[-1]])
+
+
+def plot_paths_v_time(path,beta,q_star,stoptime,t):
+    # plot the paths vs time
+    fig_x1_t, ax_x1_t = plt.subplots()
+    fig_x2_t, ax_x2_t = plt.subplots()
+    fig_p1_t, ax_p1_t = plt.subplots()
+    fig_p2_t, ax_p2_t = plt.subplots()
+    ax_x1_t.plot(t,path[:,0],label=r'$x_{1}$')
+    ax_x1_t.scatter(0.0,q_star[0])
+    ax_x1_t.scatter(stoptime,0.0)
+    ax_x1_t.set_xlabel(r'$t$')
+    ax_x1_t.set_ylabel(r'$x_{1}$')
+    ax_x1_t.legend()
+    ax_x1_t.set_title(r'$x_{1}$ vs $t$ for $R_{0}$='+str(beta))
+
+    ax_x2_t.plot(t,path[:,1],label=r'$x_{2}$')
+    ax_x2_t.scatter(0.0,q_star[1])
+    ax_x2_t.scatter(stoptime,0.0)
+    ax_x2_t.set_xlabel(r'$t$')
+    ax_x2_t.set_ylabel(r'$x_{2}$')
+    ax_x2_t.legend()
+    ax_x2_t.set_title(r'$x_{2}$ vs $t$ for $R_{0}$='+str(beta))
+
+    ax_p1_t.plot(t,path[:,2],label=r'$p_{1}$')
+    ax_p1_t.scatter(0.0,0.0)
+    ax_p1_t.scatter(stoptime,q_star[2])
+    ax_p1_t.set_xlabel(r'$t$')
+    ax_p1_t.set_ylabel(r'$p_{1}$')
+    ax_p1_t.legend()
+    ax_p1_t.set_title(r'$p_{1}$ vs $t$ for $R_{0}$='+str(beta))
+
+    ax_p2_t.plot(t,path[:,3],label=r'$p_{2}$')
+    ax_p2_t.scatter(0.0,0)
+    ax_p2_t.scatter(stoptime,q_star[3])
+    ax_p2_t.set_xlabel(r'$t$')
+    ax_p2_t.set_ylabel(r'$p_{2}$')
+    ax_p2_t.legend()
+    ax_p2_t.set_title(r'$p_{2}$ vs $t$ for $R_{0}$='+str(beta))
+
+    plt.show()
+
 
 def record_data(folder_name,beta,gamma,stoptime,int_lin_combo,numpoints,epsilon,path,q_star,r,angle,factor,t0,duration):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -147,63 +144,27 @@ if __name__=='__main__':
     #Network Parameters
     beta, gamma = 1.3, 1.0
     # epslam,epsmu = 0.4,-0.4
-    epsilon = 0.000000001
+    epsilon = 0.1
 
     lam = beta/(1+epsilon**2)
     factor = 0.75
     abserr,relerr = 1.0e-20,1.0e-13
     # ODE parameters
-    stoptime = 12.5
+    stoptime = 12.0
     numpoints = 10000
     # Create the time samples for the output of the ODE solver
     t = np.linspace(0.0,stoptime,numpoints)
     dt = 1e-3
     # Radius around eq point, shooting angle and linear combination of the the eigenvectors
-    r = 9.345e-4
+    r = 4.71349e-6
     angle = np.pi*1.0
-    duration,t0 = 5.0,3.0
+    duration,t0 = 2.0,2.0
     tf = t0 + duration
     folder_name='epslam01_epsin01_factor0_lam16_stoptime20_t001_angle10_duration01_numpoints2000'
     int_lin_combo = 1.0 # cat 0.0 epslam = 0.1 epsmu=0.1 t0=1.0 duration=2.0
     y1_0, y2_0, p1_0, p2_0, p1_star_clancy, p2_star_clancy, shot_dq_dt, J = eq_motion_mj(lam,t0,tf, factor,epsilon)
     q_star = [y1_0, y2_0, p1_star_clancy, p2_star_clancy]
     path = one_shot(angle, int_lin_combo, q_star, r, t, dt, J, shot_dq_dt,epsilon,lam,factor,gamma,duration,t0)
-    # y1_for_linear = np.linspace(path[:, 0][-1], 0, numpoints)
-    # py1_linear = p1_star_clancy - ((p1_star_clancy - path[:, 2][-1]) / path[:, 0][-1]) * y1_for_linear
-    # y2_for_linear = np.linspace(path[:, 1][-1], 0, numpoints)
-    # py2_linear = p2_star_clancy - ((p2_star_clancy - path[:, 3][-1]) / path[:, 1][-1]) * y2_for_linear
-    # addition_to_path = np.stack((y1_for_linear, y2_for_linear, py1_linear, py2_linear), axis=1)
-    # path_addition = np.vstack((path, addition_to_path))
-    # plt.plot((path_addition[:,0]+path_addition[:,1])/2,(path_addition[:,2]+path_addition[:,3])/2)
-    # plt.plot((path[:,0]+path[:,1])/2,(path[:,2]+path[:,3])/2)
-    # plt.xlabel('w')
-    # plt.ylabel(r'$p_{w}$')
-    # plt.title(r'$p_{w}$ vs w for $R_{0}$='+str(beta))
-    # plt.show()
-    # plt.plot(path[:,0],path[:,2],label='y1')
-    # plt.xlabel(r'$y_{1}$')
-    # plt.ylabel(r'$p_{1}$')
-    # plt.title(r'$p_{1}$ vs $y_{1}$ for $R_{0}$='+str(beta))
-    # plt.show()
-    # plt.plot(path[:,1],path[:,3],label='y2')
-    plt.plot(t,path[:,0],label='y2')
-    plt.scatter(0.0,y1_0)
-    plt.scatter(stoptime,0.0)
-    # plt.xlabel(r'$y_{2}$')
-    # plt.ylabel(r'$p_{2}$')
-    plt.title(r'$p_{2}$ vs $y_{2}$ for $R_{0}$='+str(beta))
-    plt.xlabel(r'$t$')
-    plt.ylabel(r'$p$')
-    plt.legend()
-    plt.title(r'$p$ vs $y$ for $R_{0}$='+str(beta))
-
-    plt.show()
-
-
-    # plt.plot(t, (path[:,0]+path[:,1])/2)
-    # plt.xlabel('t')
-    # plt.ylabel(r'$w$')
-    # plt.title(r'w vs t for $R_{0}$=' + str(beta))
-    # plt.show()
+    plot_paths_v_time(path, beta, q_star, stoptime,t)
     # record_data(folder_name, beta, gamma, stoptime, int_lin_combo, numpoints, epsilon, path, q_star, r, angle,factor,t0,duration)
     print('This no love song')
